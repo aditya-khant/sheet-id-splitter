47cadd68d559b895e429dfc76e0429ce041991e9
Evan Hubinger <evanjhub@gmail.com>
2019-02-09 16:19:46 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="score_splitter",
+    version=0.1,
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "opencv-python",
+    ],
+    packages=setuptools.find_packages(),
+)
