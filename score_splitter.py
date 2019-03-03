902e64c8f03909e20d34998e010d431493bdbe5f
Aditya Khant <33049089+aditya-khant@users.noreply.github.com>
2019-03-02 16:04:31 -0800

    cleanup magic number is 2

--- score_splitter.py
+++ score_splitter.py
@@ -350,7 +350,7 @@ class Score:
                     bars_in_this_stave += [(i, start, end)]
                 
                 if clean_up:
-                    width_magic_number = 100
+                    width_magic_number = 2
                     cleaned_up_bars = cleanup_bars(bars_in_this_stave, self._score.shape[0] // width_magic_number )
                     if cleaned_up_bars is not None:
                         self._bars_start_end += cleaned_up_bars
