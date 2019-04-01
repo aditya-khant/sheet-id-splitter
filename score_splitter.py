071c360bbbf5fb354b48a21d4260b4a30e33c322
Aditya Khant <33049089+aditya-khant@users.noreply.github.com>
2019-03-31 21:40:12 -0700

    name = main

--- score_splitter.py
+++ score_splitter.py
@@ -676,5 +676,5 @@ if __name__ == '__main__':
     # cnn_txt_staves()
     # test_bar_print(dataset="piano_dataset",output_dir='/home/ckurashige/bars_using_avg_min/', toggle='peaks')
     # test_bar_print(output_dir='/home/ckurashige/bars_using_intersections/', toggle='intersect')
-    cnn_bar_size_printout()
-
+    # cnn_bar_size_printout()
+    tsai_bar_printout()
